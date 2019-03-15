9aa3c1d5358140ac74ae165300936c14f03f0722
Kate <frayizy@gmail.com>
2019-03-15 21:25:32 +0300

    inital

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(212, 209);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 236);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(212, 15);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Tag = "999";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+		}
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+	}
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class Form1 : Form
+	{
+		Random rnd = new Random();
+		PictureBox[,] buttons = null;
+		PictureBox PrevBTN = null;
+        const int shmebulok = 4;
+		public Form1()
+		{
+			InitializeComponent();
+			TilesGen();
+		}
+
+		public void TilesGen()
+		{
+			int[] nums = new int[shmebulok * shmebulok];
+
+			for (int i = 0; i < shmebulok * shmebulok; i++)
+				nums[i] = i / 2 + 1;
+
+			for (int i = 0; i < 1000; i++)
+			{
+				int p1 = rnd.Next(shmebulok * shmebulok);
+				int p2 = rnd.Next(shmebulok * shmebulok);
+
+				int tmp = nums[p1];
+
+				nums[p1] = nums[p2];
+				nums[p2] = tmp;
+			}
+
+			buttons = new PictureBox[shmebulok, shmebulok];
+
+			Size = new Size(shmebulok * 100 + 15, shmebulok * 100 + 50);
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            for (int i = 0; i < shmebulok; i++)
+				for (int j = 0; j < shmebulok; j++)
+				{
+					PictureBox nb = new PictureBox();
+					nb.Tag = nums[i * shmebulok + j];
+
+					//nb.Text = "*";
+                    //nb.Font = new Font("Microsoft Sans Serif", 30F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(204)));
+
+                    nb.Location = new Point(i * 100, j * 100);
+					nb.Size = new Size(100,100);
+
+					nb.BackgroundImage = Properties.Resources.back;
+					nb.BackgroundImageLayout = ImageLayout.Zoom;
+
+					nb.Click += button1_Click;
+
+                    nb.Parent = this;
+
+					buttons[i, j] = nb;
+
+                    nb.BringToFront();
+				}
+
+            pictureBox1.Size = new Size(shmebulok * 100, shmebulok * 100);
+            pictureBox1.Location = new Point(0, 0);
+
+			progressBar1.Maximum = shmebulok * shmebulok * 10;
+			progressBar1.Width = Size.Width;
+			progressBar1.Location = new Point(0, Size.Height - 50);
+			progressBar1.Value = 0;
+        }
+
+		private async void button1_Click(object sender, EventArgs e)
+		{
+			if (PrevBTN == null)
+			{
+				PrevBTN = sender as PictureBox;
+				PrevBTN.BackgroundImage = Properties.Resources.ResourceManager.GetObject("img" + PrevBTN.Tag.ToString()) as Image;
+			}
+			else
+			{
+				PictureBox newBTN = sender as PictureBox;
+
+				if (newBTN.Tag.ToString() == PrevBTN.Tag.ToString())
+				{
+					newBTN.Visible = false;
+					PrevBTN.Visible = false;
+					progressBar1.Value += 20;
+                    CheckWin();
+                }
+				else
+				{
+					newBTN.BackgroundImage = Properties.Resources.ResourceManager.GetObject("img" + newBTN.Tag.ToString()) as Image;
+					await Task.Delay(500);
+					newBTN.BackgroundImage = Properties.Resources.back;
+					PrevBTN.BackgroundImage = Properties.Resources.back;
+				}
+
+				PrevBTN = null;
+			}
+        }
+
+        private void CheckWin()
+        {
+            foreach (PictureBox e in buttons)
+                if (e.Visible == true) return;
+
+            DialogResult dialogResult = MessageBox.Show("You won!", "WIN", MessageBoxButtons.RetryCancel);
+            if (dialogResult == DialogResult.Retry)
+                TilesGen();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
